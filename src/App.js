2a20470a16e04294481f36860a717da1ecbdf376
Sorrybro802 <poweshuxratov@gmail.com>
2022-02-16 18:18:33 +0500

    react birinchi vazifa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./pages/footer"
+import Header from "./pages/header"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+                <h2>HELLO WORLD</h2>
+            <Footer />
+        </div>
+    )
 }
-
 export default App;
---
+++ src/pages/footer.js
@@ -0,0 +1,10 @@
+import "./footer.css"
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h1>Footer</h1>
+        </div>
+    )
+}
+export default Footer;
